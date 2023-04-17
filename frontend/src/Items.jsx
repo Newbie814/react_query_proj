bf8de84e1b63924bc9e5e3be336b286f00aada98
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-17 18:02:00 -0400

    switched from rendering with local data to grabbing from api

--- frontend/src/Items.jsx
+++ frontend/src/Items.jsx
@@ -1,8 +1,24 @@
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+
+const Items = () => {
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
